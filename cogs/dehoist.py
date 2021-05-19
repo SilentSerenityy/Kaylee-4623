30d39add816dec00d3402c99a93a6133235d4b6c
Shadi Alostaz <66328589+SilentSerenityy@users.noreply.github.com>
2021-05-19 10:17:37 -0400

    Re-formatted De-hoist command!

--- cogs/dehoist.py
+++ cogs/dehoist.py
@@ -4,7 +4,6 @@ import discord, random, aiosqlite3
 class dehoist(commands.Cog):
   def __init__(self, client):
     self.client = client
-
   @commands.command()
   async def dehoistnickname(self,ctx,symbol):
     nick = None
@@ -14,7 +13,6 @@ class dehoist(commands.Cog):
       if nickname.startswith(obj):
         await member.edit(nick=None)
         await ctx.send(f"{member.name} has been de-hoisted!")
-
   @commands.command()
   async def dehoistname(self,ctx,symbol):
     nick = None
@@ -24,7 +22,5 @@ class dehoist(commands.Cog):
       if dispname.startswith(obj):
         await member.edit(nick=None)
         await ctx.send(f"{member.name} has been de-hoisted!")
-      
-
 def setup(client):
   client.add_cog(dehoist(client))
