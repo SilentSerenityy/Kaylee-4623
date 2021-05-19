e5d1285b47c474e32f1426f12054a70ec81f59ad
Shadi Alostaz <66328589+SilentSerenityy@users.noreply.github.com>
2021-05-19 10:10:44 -0400

    Updated Spotify command!

--- cogs/spotify.py
+++ cogs/spotify.py
@@ -6,7 +6,6 @@ from PIL import Image, ImageFont, ImageDraw
 
 import re
  
- 
 def rgb_to_hex(rgb_color):
     rgb_color = re.search('\(.*\)', rgb_color).group(0).replace(' ', '').lstrip('(').rstrip(')')
     [r, g, b, a] = [int(x) for x in rgb_color.split(',')]
@@ -39,19 +38,6 @@ class Spotify(commands.Cog):
         spotify_result = next((activity for activity in user.activities if isinstance(activity, discord.Spotify)), None)
         if spotify_result is None:
             await ctx.send(f'{user.name} is not listening to Spotify.')
-        #track_background_image = Image.open('spotify_template.png')
-        #album_image = Image.open(requests.get(spotify_result.album_cover_url, stream=True).raw).convert('RGBA')
-        #title_font = ImageFont.truetype('theboldfont.ttf', 16)
-        #artist_font = ImageFont.truetype('theboldfont.ttf', 14)
-        #album_font = ImageFont.truetype('theboldfont.ttf', 14)
-        #start_duration_font = ImageFont.truetype('theboldfont.ttf', 12)
-        #end_duration_font = ImageFont.truetype('theboldfont.ttf', 12)
-        #title_text_position = 150, 30
-        #artist_text_position = 150, 60
-        #album_text_position = 150, 80
-        #start_duration_text_position = 150, 122
-        #end_duration_text_position = 515, 122
-        #draw_on_image = ImageDraw.Draw(track_background_image)
         album_image = Image.open(requests.get(spotify_result.album_cover_url, stream=True).raw).convert('RGBA')
         album_color = album_image.getpixel((250, 100))
         print(album_color)
@@ -88,8 +74,6 @@ class Spotify(commands.Cog):
         y = x.replace('y', '4')
         z = y.replace('z', '5')
 
-
-
         color = int(z)
         url = spotify_result.album_cover_url
         embed = discord.Embed(title = f"{ctx.author.name} is currently listening to Spotify",color = color)
@@ -99,22 +83,6 @@ class Spotify(commands.Cog):
         embed.add_field(name = "Album:", value = f"{spotify_result.album}",inline = False)
         embed.add_field(name = "Duration:", value = f"{dateutil.parser.parse(str(spotify_result.duration)).strftime('%M:%S')}",inline = False)
         await ctx.send(embed = embed)
-        #draw_on_image.text(title_text_position, spotify_result.title, 'white', font=title_font)
-        #draw_on_image.text(artist_text_position, f'by {spotify_result.artist}', 'white', font=artist_font)
-        #draw_on_image.text(album_text_position, spotify_result.album, 'white', font=album_font)
-        #draw_on_image.text(start_duration_text_position, '0:00', 'white', font=start_duration_font)
-        #draw_on_image.text(end_duration_text_position,
-                           #f"{dateutil.parser.parse(str(spotify_result.duration)).strftime('%M:%S')}",
-                           #'white', font=end_duration_font)
-        #album_color = album_image.getpixel((250, 100))
-        #background_image_color = Image.new('RGBA', track_background_image.size, album_color)
-        #background_image_color.paste(track_background_image, (0, 0), track_background_image)
-        #album_image_resize = album_image.resize((140, 160))
-        #background_image_color.paste(album_image_resize, (0, 0), album_image_resize)
-        #background_image_color.convert('RGB').save('spotify.jpg', 'JPEG')
-
-        #await ctx.send(file=discord.File('spotify.jpg'))
-
 
 def setup(client):
     client.add_cog(Spotify(client))
